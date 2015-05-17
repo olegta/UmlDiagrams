c52ef09d19770f0190243ae47e2c23324c5d19d8
1 <@qw.ru>
2015-05-17 14:00:42 +0300

    Кнопка сжатия-расстяжения типа

---
+++ UmlDiagrams.WebApp/Scripts/Home/DiagramDrawing/Diagram.js
@@ -0,0 +1,77 @@
+﻿/**
+ * Diagram class
+ * @param {jQuery} divElement 
+ */
+function Diagram(divElement) {
+    var self = this;
+
+    var _divElement = divElement;
+    var _name = $("#diagram-name", _divElement).val();// TODO: init variables
+    var _width;
+    var _height;
+    var _elements = [];
+    var _arrows = [];
+
+    self.rename = function(newName) {
+        _name = newName;
+    }
+
+    self.getName = function() {
+        return _name;
+    }
+
+    self.resize = function(width, height) {
+        _width = width;
+        _height = height;
+    }
+
+    self.getWidth = function() {
+        return _width;
+    }
+
+    self.getHeight = function() {
+        return _height;
+    }
+
+    self.addElement = function(element) {
+        
+    }
+
+    self.addClass = function (classDiv, container) {
+        $("#diagram-container").append(classDiv);
+        var leftPosition = Number(classDiv.css("left").match(/\d+/)) - container.offsetLeft;
+        var topPosition = Number(classDiv.css("top").match(/\d+/)) - container.offsetTop;
+        classDiv.css({ "left": leftPosition + "px", "top": topPosition + "px" });
+        classDiv.draggable({
+            containment: "#diagram-container"
+        }).resizable({
+            handles: "all"
+        });
+
+        // support contenteditable in draggable 
+        $(".type-title", classDiv).dblclick(function () {
+            console.log("enable");
+            classDiv.draggable({ disabled: true });
+            $(this).focus();
+        });
+        classDiv.click(function () {
+            console.log("disable");
+            classDiv.draggable({ disabled: false });
+        });
+    }
+}
+
+function DrawingState() { };
+
+DrawingState.prototype.act = function() {
+    throw new Error("Not implemented");
+};
+
+function PointerState() { };
+PointerState.prototype = Object.create(DrawingState);
+PointerState.prototype.act = function () { };
+
+function ClassAdding() { }
+
+ClassAdding.prototype = Object.create(DrawingState);
+
--- UmlDiagrams.WebApp/Scripts/Home/DiagramDrawing/initialization.js
+++ UmlDiagrams.WebApp/Scripts/Home/DiagramDrawing/initialization.js
@@ -3,26 +3,63 @@
 
     /** @const */
     var CREATING_ELEMENT_CLASS = "dragging-to-diagram";
+    /** @const */
+    var TOOLBOX_POINTER_CLASS = "icon-pointer";
+    /** @const */
+    var TOOLBOX_CLASS_CLASS = "icon-class";
+    /** @const */
+    var TOOLBOX_INTERFACE_CLASS = "icon-interface";
+    /** @const */
+    var TOOLBOX_ENUMERATION_CLASS = "icon-enumeration";
+    /** @const */
+    var TOOLBOX_COMMENT_CLASS = "icon-comment";
+    /** @const */
+    var TOOLBOX_ASSOCIATION_CLASS = "icon-association";
+    /** @const */
+    var TOOLBOX_AGGREGATION_CLASS = "icon-aggregation";
+    /** @const */
+    var TOOLBOX_COMPOSITION_CLASS = "icon-composition";
+    /** @const */
+    var TOOLBOX_DEPENDENCY_CLASS = "icon-dependency";
+    /** @const */
+    var TOOLBOX_INHERITANCE_CLASS = "icon-inheritance";
+    /** @const */
+    var TOOLBOX_CONNECTOR_CLASS = "icon-connector";
+
+
+    var diagram = new Diagram();
+
 
     var toolboxItems = $("#uml-elements-items").children();
-    toolboxItems.bind("mousemove", function() {
+    toolboxItems.bind("mousemove", function () {
         var selected = this;
         if (!$(selected).hasClass("toolbox-element-selected"))
             $(selected).addClass("toolbox-element-over");
-        toolboxItems.each(function() {
+        toolboxItems.each(function () {
             if (this !== selected)
                 $(this).removeClass("toolbox-element-over");
         });
     }).bind("mouseout", function() {
         $(this).removeClass("toolbox-element-over");
-    }).bind("click", function() {
+    }).bind("mousedown", function () {
         var selected = this;
         $(selected).addClass("toolbox-element-selected");
-        toolboxItems.each(function() {
+        toolboxItems.each(function () {
             $(this).removeClass("toolbox-element-over");
             if (this !== selected)
                 $(this).removeClass("toolbox-element-selected");
-        });
+        });        
+    }).first().trigger("mousedown");
+
+    $("." + TOOLBOX_POINTER_CLASS).addClass(CREATING_ELEMENT_CLASS);
+
+    $(".icon-class").parent().draggable({
+        helper: function () {
+            return $("#class-clone").clone().css("display", "")
+                .removeAttr("id")
+                .addClass(CREATING_ELEMENT_CLASS);
+        },
+        cursorAt: { left: 0 }
     });
 
     $("#diagram-container").droppable({
@@ -30,39 +67,49 @@
         drop: function (event, ui) {
             if (!ui.helper.hasClass(CREATING_ELEMENT_CLASS))
                 return;
-            var helperCopy = ui.helper.clone();
-            helperCopy.removeClass(CREATING_ELEMENT_CLASS);
-            $("#diagram-container").append(helperCopy);
-            var leftPosition = Number(helperCopy.css("left").match(/\d+/)) - this.offsetLeft;
-            var topPosition = Number(helperCopy.css("top").match(/\d+/)) - this.offsetTop;
-            helperCopy.css({ "left": leftPosition + "px", "top": topPosition + "px" });
-            helperCopy.draggable({
-                containment: "#diagram-container"
-            }).resizable({
-                handles: "all"
-            });
+            console.log("add class");
+            var classDiv = ui.helper.clone();
+            classDiv.removeClass(CREATING_ELEMENT_CLASS);
+            diagram.addClass(classDiv, this);
 
-            // support contenteditable in draggable 
-            $(".type-title", helperCopy).dblclick(function () {
-                console.log("enable");
-                helperCopy.draggable({ disabled: true });
-                $(this).focus();
-            });
-            helperCopy.click(function () {
-                console.log("disable");
-                helperCopy.draggable({ disabled: false });
-            });
+            if (ui.helper.hasClass(TOOLBOX_CLASS_CLASS)) {
+                console.log("add class");
+                diagram.addClass(ui.helper.clone()).call(this);
+            } else if (ui.helper.hasClass(TOOLBOX_INTERFACE_CLASS)) {
+                diagram.addClass(ui.helper.clone());
+            } else if (ui.helper.hasClass(TOOLBOX_ENUMERATION_CLASS)) {
+                diagram.addClass(ui.helper.clone());
+            } else if (ui.helper.hasClass(TOOLBOX_COMMENT_CLASS)) {-
+                diagram.addClass(ui.helper.clone());
+            } else if (ui.helper.hasClass(TOOLBOX_ASSOCIATION_CLASS)) {
+                diagram.addClass(ui.helper.clone());
+            } else if (ui.helper.hasClass(TOOLBOX_AGGREGATION_CLASS)) {
+                diagram.addClass(ui.helper.clone());
+            } else if (ui.helper.hasClass(TOOLBOX_COMPOSITION_CLASS)) {
+                diagram.addClass(ui.helper.clone());
+            } else if (ui.helper.hasClass(TOOLBOX_DEPENDENCY_CLASS)) {
+                diagram.addClass(ui.helper.clone());
+            } else if (ui.helper.hasClass(TOOLBOX_INHERITANCE_CLASS)) {
+                diagram.addClass(ui.helper.clone());
+            } else if (ui.helper.hasClass(TOOLBOX_CONNECTOR_CLASS)) {
+                diagram.addClass(ui.helper.clone());
+            }
         }
     }).resizable({
         
+    }).click(function () {
+        
     });
-
-    $(".icon-class").parent().draggable({
-        helper: function() {
-            return $("#class-clone").clone().css("display", "")
-                .removeAttr("id")
-                .addClass(CREATING_ELEMENT_CLASS);
-        },
-        cursorAt: { left: 0 }
+    $(document).on("click", ".collapse-icon,.expand-icon", function () {
+        var typeContainer = $(this).closest(".type-container");
+        if ($(this).hasClass("collapse-icon")) {
+            typeContainer.animate({ height: $(this).parent().outerHeight(true) + "px" });
+        } else if ($(this).hasClass("expand-icon")) {
+            typeContainer.animate({ height: typeContainer[0].scrollHeight });
+        } else {
+            throw new Error("invalid state of expand-collapse icon");
+        }
+        $(this).toggleClass("collapse-icon");
+        $(this).toggleClass("expand-icon");
     });
 });
